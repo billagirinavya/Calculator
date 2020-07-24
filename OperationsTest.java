add03d1d3aa40e73fe8790aa2b78cb14f6b2724c
billagirinavya <60705200+billagirinavya@users.noreply.github.com>
2020-07-24 12:13:27 +0530

    Add files via upload

---
+++ OperationsTest.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OperationsTest {
+
+	@Test
+	/*
+	 * TestCases for Addition
+	 */
+	public void testAdd() {
+		
+		Operations o = new Operations();
+		assertEquals(o,o.add(5, 3));
+		assertEquals(o,o.add(3, -9));
+		assertEquals(o,o.add(-5, -9));
+	}
+	
+	@Test
+	/*
+	 * Test Cases for Multiplication
+	 */
+	public void testMul() {
+		Operations o = new Operations();
+		assertEquals(o,o.mul(5, 7));
+		assertEquals(o,o.mul(-3, 7));
+		assertEquals(o,o.mul(-5, -4));
+	}
+	@Test
+	/*
+	 * Test Cases for Division
+	 */
+	public void testDiv() {
+		Operations o = new Operations();
+		assertEquals(o,o.div(15, 5));
+		assertEquals(o,o.div(0, 0));
+		assertEquals(o,o.div(15, 0));
+		assertEquals(o,o.div(9, 99));
+	}
+}
